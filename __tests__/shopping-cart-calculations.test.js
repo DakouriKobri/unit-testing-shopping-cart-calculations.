e824b87011253af4b849894ec661405751783a89
Dakouri M. Kobri <maurille.kobri@gmail.com>
2024-02-17 17:56:03 +0100

    Calculate total cart price without discount

--- __tests__/shopping-cart-calculations.test.js
+++ __tests__/shopping-cart-calculations.test.js
@@ -1,5 +1,8 @@
 // Local Files
-const { calculateTotalQuantity } = require('../shopping-cart-calculations');
+const {
+  calculateTotalPrice,
+  calculateTotalQuantity,
+} = require('../shopping-cart-calculations');
 
 describe('calculateTotalQuantity', () => {
   it('calculates total items quantity', () => {
@@ -9,5 +12,16 @@ describe('calculateTotalQuantity', () => {
     ];
     const totalQuantity = calculateTotalQuantity(cart);
     expect(totalQuantity).toBe(13);
+  });
+});
+
+describe('calculateTotalPrice', () => {
+  it('calculates total price without discount', () => {
+    const cart = [
+      { item: 'ginger juice', price: 4.99, quantity: 3 },
+      { item: 'apple juice', price: 9.99, quantity: 10 },
+    ];
+    const totalPrice = calculateTotalPrice(cart);
+    expect(totalPrice).toBe(114.87);
   });
 });
--- shopping-cart-calculations.js
+++ shopping-cart-calculations.js
@@ -2,4 +2,10 @@ function calculateTotalQuantity(cart) {
   return cart.reduce((sum, item) => sum + item.quantity, 0);
 }
 
-module.exports = { calculateTotalQuantity };
+function calculateTotalPrice(cart) {
+  return Number(
+    cart.reduce((sum, item) => sum + item.quantity * item.price, 0).toFixed(2)
+  );
+}
+
+module.exports = { calculateTotalPrice, calculateTotalQuantity };
